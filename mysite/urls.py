1f12f65bdec95ff44ebd47aa7c3465471e001e7d
NapatrSaengthongsakullert <napatr.se@ku.th>
2024-11-24 21:58:42 +0700

    Push docker to the KU-POLLS

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth.views import LoginView, LogoutView
-
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/polls/', permanent=True)),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
